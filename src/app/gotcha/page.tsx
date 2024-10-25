ce6d0e319bc2c152e4eaca806de228784ce0308f
Keanu Husen <keanu@plainjoe.net>
2024-10-25 18:47:37 -0500

    adding select

--- src/app/gotcha/page.tsx
+++ src/app/gotcha/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+// import ContactForm from "@/components/form";
 
 export default function Home() {
   return (
@@ -17,7 +18,7 @@ export default function Home() {
           </div>
           <div className="basis-full grow-0 shrink-0 md:basis-1/2">
             <h1 className="text-5xl font-display text-[#E94B3C] underline md:text-7xl">gotcha !!</h1>
-            <div className="mt-4 text-xl text-[#007E80]">
+            <div className="mt-4 text-lg text-[#007E80] md:text-xl">
               <p>you have officially been got !!!</p>
               <p>hehehe loser.</p>
             </div>
@@ -26,12 +27,34 @@ export default function Home() {
         <div className="block pt-8 max-w-3xl m-auto">
           <div className="basis-1/2 grow-0 shrink-0 text-pretty">
             <h1 className="text-2xl font-display text-[#E94B3C] md:text-4xl">What can be done with such an atrocity?!</h1>
-            <div className="mt-4 text-xl text-[#007E80]">
+            <div className="mt-4 text-lg text-[#007E80] md:text-xl">
               <p>Have no fear. Your time has been wasted... yes, it has indeed been wasted. Yet you are still reading this paragraph as if it will miraculously bring about redemption for your lost time. It will not... but there is something you can do to redeem your ego and... dare I say, your pride!</p>
               <br />
               <p>You can fill out the survey below!</p>
               <br />
               <p>All joking laid aside, the true intent for this QR code was science. You have so graciously scanned and been led to the primary purpose of this website which is research and not development. So please, when answering the survey, keep in mind that this is for posterity.</p>
+              <br />
+              {/* <p>[form will go here eventually]</p> */}
+              <form action="alert('fake form fill... gotcha again!')">
+                <label htmlFor="">Where did you scan this QR code?</label>
+                <br />
+                <select name="" id="">
+                  <option value="">Atlanta</option>
+                  <option value="">Birmingham</option>
+                  <option value="">Chicago</option>
+                  <option value="">Dallas/Fort Worth</option>
+                  <option value="">Houston</option>
+                  <option value="">Los Angeles</option>
+                  <option value="">Nashville</option>
+                  <option value="">New York</option>
+                  <option value="">Orlando</option>
+                  <option value="">Phoenix</option>
+                  <option value="">San Diego</option>
+                  <option value="">San Francisco/San Jose</option>
+                  <option value="">Seattle</option>
+                </select>
+              </form>
+              {/* <ContactForm /> */}
             </div>
           </div>
         </div>
