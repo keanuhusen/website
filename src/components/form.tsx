86fa4ace80c857f1d00bba5d04b879b21c2cfc9c
Keanu Husen <keanu@plainjoe.net>
2024-10-25 18:51:40 -0500

    removing formspree

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,47 +1,47 @@
-'use client';
-import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
+// 'use client';
+// import React from 'react';
+// import { useForm, ValidationError } from '@formspree/react';
 
-function ContactForm() {
-  const [state, handleSubmit] = useForm("xzzbzlny");
-  if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
-  }
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
-  );
-}
-
-// function App() {
+// function ContactForm() {
+//   const [state, handleSubmit] = useForm("xzzbzlny");
+//   if (state.succeeded) {
+//       return <p>Thanks for joining!</p>;
+//   }
 //   return (
-//     <ContactForm />
+//     <form onSubmit={handleSubmit}>
+//       <label htmlFor="email">
+//         Email Address
+//       </label>
+//       <input
+//         id="email"
+//         type="email" 
+//         name="email"
+//       />
+//       <ValidationError 
+//         prefix="Email" 
+//         field="email"
+//         errors={state.errors}
+//       />
+//       <textarea
+//         id="message"
+//         name="message"
+//       />
+//       <ValidationError 
+//         prefix="Message" 
+//         field="message"
+//         errors={state.errors}
+//       />
+//       <button type="submit" disabled={state.submitting}>
+//         Submit
+//       </button>
+//     </form>
 //   );
 // }
 
-export default ContactForm;
+// // function App() {
+// //   return (
+// //     <ContactForm />
+// //   );
+// // }
+
+// export default ContactForm;
